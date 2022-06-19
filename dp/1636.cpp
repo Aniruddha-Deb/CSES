48ce29ffb1dd0c8f78103a209f16db90286f899d
Aniruddha-Deb <aniruddha.deb.2002@gmail.com>
2022-06-19 21:22:23 +0530

    Coin counting I & II

---
+++ dp/1636.cpp
@@ -0,0 +1,35 @@
+// Consider a money system consisting of n coins. Each coin has a positive 
+// integer value. Your task is to calculate the number of ordered ways you can 
+// produce a money sum x using the available coins.
+ 
+#include <bits/stdc++.h>
+ 
+#define MOD 1000000007
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int n, x;
+    cin >> n >> x;
+ 
+    vector<int> A(n);
+    for (int i=0; i<n; i++) cin >> A[i];
+ 
+    vector<long long> DP(x+1,0);
+ 
+    DP[0] = 1;
+
+    for (int j: A) {
+        for (int i=1; i<=x; i++) {
+            if (i-j >= 0) {
+                DP[i] += DP[i-j];
+                DP[i] %= MOD;
+            }
+        }
+    }
+ 
+    cout << DP[x] << endl;
+ 
+    return 0;
+}
